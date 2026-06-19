3387fbfe6d03314fe5a6b74f69619006d845c73e
yuto <yuto57taira@gmail.com>
2026-06-19 18:45:39 +0900

    トークン修正対応

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  url: (process.env.UPSTASH_REDIS_REST_URL ?? process.env.KV_REST_API_URL)!,
+  token: (process.env.UPSTASH_REDIS_REST_TOKEN ?? process.env.KV_REST_API_TOKEN)!,
 });
